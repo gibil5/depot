d87decd198f3948e232a31e798f2455aa15c72f7
Javier Revilla <jrevilla.pcm@gmail.com>
2020-01-04 12:15:59 -0500

    Ghost Testing Finished

--- app/controllers/ghosts_controller.rb
+++ app/controllers/ghosts_controller.rb
@@ -1,2 +1,41 @@
 class GhostsController < ApplicationController
+
+
+
+	# Show
+	def show
+		puts
+		puts('Reniec - Show')
+		
+		puts(params)
+		#puts(params[:message])
+		#puts(params[:format])
+
+
+		# Init
+		dni='09817194'
+
+
+		# New
+		#reniec_creator = ReniecCreator.new(params[:message])
+		reniec_creator = ReniecCreator.new(dni)
+
+		
+
+		#reniec_creator.send_dni()
+		#@data_first, @data_second = reniec_creator.send_dni()
+		@data_first, @data_second, @data_names, @data_address, @data_status, @data_restrictions, @data_ubigeo, @data_photo = reniec_creator.send_dni()
+
+
+		puts('mark 11')
+		puts(@data_first)
+		puts(@data_second)
+
+
+	
+		# Dep
+		#send_reniec(dni)
+	end
+	
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   resources :ghosts
 
+  get 'ghosts/reniec'
+
 end
